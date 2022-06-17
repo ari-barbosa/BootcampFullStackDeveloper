e178a3fc5fc0b2cf01a6eac0e2cf263af75d43df
Ariane Barbosa <88691922+ari-barbosa@users.noreply.github.com>
2022-06-16 22:38:40 -0300

    Atualizando o modulo JavaScript

---
+++ JavaScript/teste.js
@@ -0,0 +1,18 @@
+// CRIANDO COMENTÁRIOS
+
+// var y = 0;
+
+/* var x = 1
+   var z =2 */
+
+// ctrl + / -> comentário de linha
+
+
+// CRIANDO VARIAVEIS
+var preco = 2;
+var desconto = 0.2;
+preco = preco - desconto
+const PRECO1 = 3 // CONSTANTE EM MAIUSCULO
+
+
+
