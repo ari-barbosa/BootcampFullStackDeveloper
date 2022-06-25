708631177c176e2299b2bc0a8367cd4a9791a645
Ariane Barbosa <abarbosa.ipms@gmail.com>
2022-06-25 18:39:44 -0300

    Novo módulo e projeto de JS finalizado

---
+++ JavaScript/proj-modulo/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Ariane", 21));
+console.log(mostraCidade("Ariane", "Sumaré"));
+console.log(mostraHobby("Ariane", "Leitura"));
